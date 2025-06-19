a0a51a824e016e35ddd59466670c9d90ee19e2a2
MijersOne <63684892+MijersOne@users.noreply.github.com>
2025-06-18 22:36:42 -0400

    Fix AddClothing table widget name and add destructor

--- addclothing.cpp
+++ addclothing.cpp
@@ -8,24 +8,30 @@ AddClothing::AddClothing(const QString &clothingType, QWidget *parent)
     ui->setupUi(this);
 
     // Set up the table with 5 rows and 2 columns
-    ui->clothingTable->setRowCount(5);
-    ui->clothingTable->setColumnCount(2);
-    ui->clothingTable->setHorizontalHeaderLabels(QStringList() << "Attribute" << "Value");
+    // Set up the table widget which is named "tw_addcloth" in the UI file
+    ui->tw_addcloth->setRowCount(5);
+    ui->tw_addcloth->setColumnCount(2);
+    ui->tw_addcloth->setHorizontalHeaderLabels(QStringList() << "Attribute" << "Value");
 
     QStringList attributes = {"Type", "Name", "Colour", "Style", "Description"};
     for (int row = 0; row < attributes.size(); ++row) {
         QTableWidgetItem *attrItem = new QTableWidgetItem(attributes[row]);
         attrItem->setFlags(attrItem->flags() & ~Qt::ItemIsEditable);  // Make attribute column read-only
-        ui->clothingTable->setItem(row, 0, attrItem);
+        ui->tw_addcloth->setItem(row, 0, attrItem);
 
         QTableWidgetItem *valueItem = new QTableWidgetItem();
         if (attributes[row].toLower() == "type") {
             valueItem->setText(clothingType);
             valueItem->setFlags(valueItem->flags() & ~Qt::ItemIsEditable);  // Lock type cell too
         }
-        ui->clothingTable->setItem(row, 1, valueItem);
+        ui->tw_addcloth->setItem(row, 1, valueItem);
     }
 
-    ui->clothingTable->horizontalHeader()->setStretchLastSection(true);
-    ui->clothingTable->verticalHeader()->setVisible(false);
+    ui->tw_addcloth->horizontalHeader()->setStretchLastSection(true);
+    ui->tw_addcloth->verticalHeader()->setVisible(false);
+}
+
+AddClothing::~AddClothing()
+{
+    delete ui;
 }
