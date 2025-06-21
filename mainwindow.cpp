cf16fe0d67e8487754a992b800e73f2b8868a6a1
MijersOne <63684892+MijersOne@users.noreply.github.com>
2025-06-20 21:31:12 -0400

    Add dynamic Report submenu

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "assignments.h"
+#include "addclothing.h"
 #include "ui_mainwindow.h"
+#include <QTimer>
 
 
 MainWindow::MainWindow(QWidget *parent)
@@ -8,7 +10,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    reportMenu = ui->menuReport;
     connect(ui->menuAssignments, &QMenu::triggered, this, &MainWindow::openAssignments);
+    QTimer::singleShot(0, this, &MainWindow::populateReportMenu);
 }
 
 MainWindow::~MainWindow()
@@ -25,4 +29,21 @@ void MainWindow::openAssignments()
         assignments = new Assignments(this);
     }
     assignments->show();
+}
+
+void MainWindow::openAddClothingDialog()
+{
+    AddClothing dlg(this, tr("Generic"));
+    dlg.exec();
+}
+
+void MainWindow::populateReportMenu()
+{
+    if (!reportMenu)
+        return;
+
+    reportMenu->clear();
+
+    QAction *addAct = reportMenu->addAction(tr("Add Clothing"));
+    connect(addAct, &QAction::triggered, this, &MainWindow::openAddClothingDialog);
 }
