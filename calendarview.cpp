1ee33baee0664f7a6d5805f04e587e5cc732ea46
MijersOne <63684892+MijersOne@users.noreply.github.com>
2025-07-06 16:16:31 -0400

    Remove list widget from calendar

--- calendarview.cpp
+++ calendarview.cpp
@@ -1,18 +1,14 @@
 #include "calendarview.h"
 #include "ui_calendarview.h"
 #include <QCalendarWidget>
-#include <QListWidget>
+#include <QSizePolicy>
 
 CalendarView::CalendarView(CyberDom *app, QWidget *parent)
     : QDialog(parent), ui(new Ui::CalendarView), mainApp(app)
 {
     ui->setupUi(this);
-    if (mainApp)
-        events = mainApp->getCalendarEvents();
-    connect(ui->calendarWidget, &QCalendarWidget::selectionChanged, this, [this]() {
-        onDateSelected(ui->calendarWidget->selectedDate());
-    });
-    onDateSelected(ui->calendarWidget->selectedDate());
+    // Fill the dialog with the calendar widget
+    ui->calendarWidget->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
 }
 
 CalendarView::~CalendarView()
@@ -20,21 +16,4 @@ CalendarView::~CalendarView()
     delete ui;
 }
 
-void CalendarView::populateList(const QDate &date)
-{
-    ui->listWidget->clear();
-    for (const CalendarEvent &ev : events) {
-        if (ev.start.date() <= date && date <= ev.end.date()) {
-            QString timeStr = ev.start.time().toString("hh:mm");
-            if (ev.start != ev.end)
-                timeStr += QString("-%1").arg(ev.end.time().toString("hh:mm"));
-            ui->listWidget->addItem(timeStr + " " + ev.title + " (" + ev.type + ")");
-        }
-    }
-}
-
-void CalendarView::onDateSelected(const QDate &date)
-{
-    populateList(date);
-}
 
